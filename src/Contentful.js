d530b1250e7da56991dbe6fd9decea3cf03a6a7e
vishal pawar <vishalp1@pune.smartek21.st21>
2019-10-07 23:59:37 +0530

    first commit

---
+++ src/Contentful.js
@@ -0,0 +1,9 @@
+const contentful = require("contentful");
+
+export default contentful.createClient({
+  // space: process.env.REACT_APP_API_SPACE,
+  // accessToken: process.env.REACT_APP_ACCESS_TOKEN
+
+  accessToken: "8xMEzdA1nJ5eAUY_5LZtFGxMY9ErX50AJI1zKBSHfPI",
+  space: "sdy2gdzsbix9"
+});
